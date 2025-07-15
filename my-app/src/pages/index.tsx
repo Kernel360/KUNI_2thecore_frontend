2a2a967f75e3e8d39d47d5dfe6b0390feecbb8b1
이총명 <dlchdaud04@naver.com>
2025-07-15 16:55:00 +0900

    폴더구조 정상화

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
-    <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <h1>메인 페이지</h1>
+    <div>
+      <Typography variant="h1" component="h2">
+        Hello, Material UI!
+      </Typography>
     </div>
   );
 }
