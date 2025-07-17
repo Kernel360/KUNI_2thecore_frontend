de5ad62f9279cac115c8f06f1d29b3624fda3871
이총명 <dlchdaud04@naver.com>
2025-07-17 14:48:09 +0900

    feat: login으로 라우트되는거 성공

---
+++ my-app/src/app/login/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "../../components/ui/card"
+
+export default function Login() {
+    return (
+      <div>
+        <div>
+        <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+        </div>
+      </div>
+    );
+  } 
