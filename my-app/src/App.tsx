89f825443f04da67444e18714998da4702be308c
이총명 <dlchdaud04@naver.com>
2025-07-17 11:03:46 +0900

    vite로 마이그레이션 시도

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,13 @@
+// 이 파일은 Vite 구조에 맞게 App.tsx로 이름이 변경될 예정입니다.
 import React from "react";
+import '../styles/globals.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Typography from '@mui/material/Typography';
 import TopBar from '../components/TopBar/TopBar';
 
-export default function Home() {
+function App() {
   return (
     <div>
       <div>
@@ -15,3 +16,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default App;
