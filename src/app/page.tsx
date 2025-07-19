b4630bccc2d01d86b83755f4ed87f51f2182273b
이총명 <dlchdaud04@naver.com>
2025-07-19 18:39:06 +0900

    feat: searchPage 라우팅 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,9 @@ export default function Home() {
   return (
     <div>
       <TopBar title="차량 관제 시스템"></TopBar>
-      <UserBox />
-      <div style={{
-        display: 'flex', justifyContent: 'center', width: '100%', gap: '35px', marginTop: '20px'
-      }}>
-        <StatusBox num={100} text="전체 차량" />
-        <StatusBox num={57} text="운행 중" />
-        <StatusBox num={13} text="대기 중" />
-        <StatusBox num={50} text="수리 중" />
-      </div>
-      <div className={styles.mapContainer}>
-        <Map width="500px" height="400px" />
+      <div style={{ margin: '0px 115px' }}>
+        <div style={{ backgroundColor: '#f6f6f6', zIndex: 100, padding: '0px 15px 100px 15px', marginTop: '3px' }}>
+        </div>
       </div>
     </div >
   );
