9251e7f3009b121d32c24360a0507200ce0b308f
suuuujinnnn <suuuujinnnn@gmail.com>
2025-07-19 19:04:36 +0900

    import 경로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,15 @@
 import Link from 'next/link';
-<<<<<<< HEAD
-import TopBar from './components/ui/topBar';
-import UserBox from './components/userBox';
-import StatusBox from './components/statusBox/statusBox';
-import Map from './components/map';
+import TopBar from '../components/ui/topBar';
+import UserBox from '../components/userBox/userBox';
+import StatusBox from '../components/statusBox/statusBox';
+import Map from '../components/map';
 import styles from './components/map.module.css';
-=======
-import TopBar from '@/components/ui/topBar';
-import UserBox from '@/components/userBox/userBox';
-import StatusContainer from '@/components/statusBox/statusContainer';
-import MenuBox from '@/components/menuBox/menuBox';
->>>>>>> HomePage
 
 export default function Home() {
   return (
-
-    <div className="flex flex-col h-screen">
+    <div>
       <TopBar title="차량 관제 시스템"></TopBar>
-<<<<<<< HEAD
       <UserBox />
-<<<<<<< HEAD
-    </div >
-=======
       <div style={{
         display: 'flex', justifyContent: 'center', width: '100%', gap: '35px', marginTop: '20px'
       }}>
@@ -34,16 +22,5 @@ export default function Home() {
         <Map width="500px" height="400px" />
       </div>
     </div >
->>>>>>> 012f24f (카카오맵 API로 지도 구현)
-=======
-      <div style={{ margin: '0px 115px' }}>
-        <div style={{backgroundColor: '#f6f6f6' ,zIndex: 100, padding:'0px 15px 100px 15px', marginTop:'3px'}}>
-        <UserBox />
-        <StatusContainer />
-        <MenuBox></MenuBox>
-      </div>
-      </div>
-    </div>
->>>>>>> HomePage
   );
 }
