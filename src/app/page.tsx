17fd1988857d92e27e0788a11c634e7808bd80a2
이총명 <dlchdaud04@naver.com>
2025-07-19 16:01:09 +0900

    feat: HomePage greyBox 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,11 @@ export default function Home() {
     <div className="flex flex-col h-screen">
       <TopBar title="차량 관제 시스템"></TopBar>
       <div style={{ margin: '0px 115px' }}>
-
+        <div style={{backgroundColor: '#f6f6f6' ,zIndex: 100, padding:'0px 15px 100px 15px', marginTop:'3px'}}>
         <UserBox />
         <StatusContainer />
         <MenuBox></MenuBox>
+      </div>
       </div>
     </div>
   );
