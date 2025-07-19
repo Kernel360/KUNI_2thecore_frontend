33d762089d8a6285fa60b5d7ac3a526c58d21f17
이총명 <dlchdaud04@naver.com>
2025-07-19 15:46:52 +0900

    Squashed commit of the following:
    commit ff080efe121b6028139b499c81b8352889e6abea
    Merge: a23c6bb f88523b
    Author: 이총명 <dlchdaud04@naver.com>
    Date:   Sat Jul 19 15:46:24 2025 +0900
    
        Merge branch '#3' into HomePage
    
    commit f88523bf88814026d28adbdd12524d847d7c5988
    Author: 이총명 <dlchdaud04@naver.com>
    Date:   Sat Jul 19 15:45:26 2025 +0900
    
        feat: menuBox 완성
    
    commit 07987f1adec4127bdc3397158f08be156d8372e1
    Author: 이총명 <dlchdaud04@naver.com>
    Date:   Sat Jul 19 13:01:05 2025 +0900
    
        fix: 사이드바 제거
    
    commit 2f37477a3293ae54558276c6bc76be66d0f9909c
    Author: 이총명 <dlchdaud04@naver.com>
    Date:   Sat Jul 19 12:49:54 2025 +0900
    
        feat: 사이드바? 개발
    
    commit a23c6bb3625cf96139888f4e721f82b9ac650033
    Author: 이총명 <dlchdaud04@naver.com>
    Date:   Sat Jul 19 11:00:19 2025 +0900
    
        nodemodules 변동

--- src/components/userBox/userBox.tsx
+++ src/components/userBox/userBox.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Button } from "./ui/button"
+import { Button } from "@/components/ui/button"
 import styles from './userBox.module.css'
 
 const UserBox = () => {
   return (
     <div className={styles.div}>
       <h5>관리자님</h5>
-      <Button className={styles.Button}>로그아웃</Button>
+      <div className="flex justify-center">
+        <Button className={styles.Button}>로그아웃</Button>
+      </div>
     </div>
   )
 }
