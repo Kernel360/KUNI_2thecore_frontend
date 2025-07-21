32ecc985058e816e441a96aebe8d9c406b139e99
이총명 <dlchdaud04@naver.com>
2025-07-21 11:10:35 +0900

    feat: carSearch input, 검색 버튼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,17 @@ import styles from '../components/map.module.css';
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <div>
+=======
+
+    <div className="flex flex-col h-screen">
+      
+>>>>>>> 3552754 (feat: carSearch input, 검색 버튼)
       <TopBar title="차량 관제 시스템"></TopBar>
+      <StatusContainer />
+      <MenuBox></MenuBox>
+
       <div style={{ margin: '0px 115px' }}>
         <div style={{ backgroundColor: '#f6f6f6', zIndex: 100, padding: '0px 15px 100px 15px', marginTop: '3px' }}>
         </div>
