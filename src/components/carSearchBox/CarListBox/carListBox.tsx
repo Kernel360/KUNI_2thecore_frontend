2a4a7f90130f09be0a91c94b5f0aa07d532c0e43
이총명 <dlchdaud04@naver.com>
2025-07-24 10:59:53 +0900

    feat: route 추가

--- src/components/carSearchBox/CarListBox/carListBox.tsx
+++ src/components/carSearchBox/CarListBox/carListBox.tsx
@@ -1,5 +1,6 @@
 import Status from "../status";
 import styles from "./carListBox.module.css";
+import Link from "next/link";
 
 interface CarListBoxProps {
     num: string;
@@ -12,12 +13,16 @@ interface CarListBoxProps {
 const CarListBox: React.FC<CarListBoxProps> = ({ num, model, brand, location, status }) => {
     return (
         <div className={styles.container}>
+            <Link href={`/DetailPage/`}>
             <div className={styles.info}>
                 <div className={styles.num}>{num}</div>
                 <div className={styles.texts}>{brand} {model} {location}</div>
             </div>
+            </Link>
             <Status status={status as "운행중" | "대기중" | "수리중"} />
+            
         </div>
+        
     )
 }
 
