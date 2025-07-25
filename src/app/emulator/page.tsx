ce64bf96b6ecabb4a06f56b0bb1c08289eb23a97
suuuujinnnn <suuuujinnnn@gmail.com>
2025-07-25 19:36:01 +0900

    에뮬레이터 페이지 1차 완성

---
+++ src/app/emulator/page.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import TopBar from '@/components/ui/topBar';
+import React from 'react';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+const dummyEmuls = [
+    { car_number: '12가 3456', emulator_id: 'EMU001', emulator_status: 'on' },
+    { car_number: '34나 5678', emulator_id: 'EMU002', emulator_status: 'on' },
+    { car_number: '56다 7890', emulator_id: 'EMU003', emulator_status: 'off' },
+];
+
+const CarEmulNumberSearchBox = () => {
+    return (
+        <div style={{
+            display: "flex", flexDirection: "column", alignItems: "center",
+            justifyContent: "center", width: "100%", marginBottom: "21px"
+        }}>
+            <div style={{
+                display: "flex", flexDirection: "row", alignItems: "center",
+                justifyContent: "center", width: "100%", marginTop: "21px",
+            }}>
+                <Input type="text" placeholder="차량 번호" style={{
+                    width: "80%", height: "38.5px",
+                    backgroundColor: "white",
+                }} />
+                <Button style={{
+                    width: "7%", height: "44px", marginLeft: "14px",
+                    backgroundColor: "#0070f3", color: "white",
+                    fontSize: "14px",
+                    fontWeight: 500,
+                }}>검색</Button>
+            </div>
+            <div style={{
+                display: "flex", flexDirection: "row", alignItems: "center",
+                justifyContent: "center", width: "100%", marginTop: "21px",
+            }}>
+                <Input type="text" placeholder="새 에뮬레이터를 등록하려면 차량 번호를 이곳에 입력해주세요." style={{
+                    width: "80%", height: "38.5px",
+                    backgroundColor: "white",
+                }} />
+                <Button style={{
+                    width: "7%", height: "44px", marginLeft: "14px",
+                    backgroundColor: "#0070f3", color: "white",
+                    fontSize: "14px",
+                    fontWeight: 500,
+                }}>등록</Button>
+            </div>
+        </div>
+    );
+};
+
+export default function Emulator() {
+    return (
+        <div>
+            <TopBar title="에뮬레이터"></TopBar>
+            <CarEmulNumberSearchBox />
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">차량번호</TableHead>
+                        <TableHead className="w-[100px]">에뮬레이터 ID</TableHead>
+                        <TableHead className="w-[100px]">ON/OFF</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">12가 3456</TableCell>
+                        <TableCell className="w-[100px]">EMU001</TableCell>
+                        <TableCell className="w-[100px]">ON</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
