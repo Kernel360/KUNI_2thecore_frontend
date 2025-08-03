e7892f74eda552df27c078dd6683012654976667
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-03 18:55:36 +0900

    HistoryPage UI 1차 완성

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,15 +1,26 @@
 'use client';
 
-import TopBar from '@/components/ui/topBar';
 import HistorySearchBox from '@/components/search-box/history-search-box';
 import HistoryListBox from '@/components/search-box/list-box/history-list-box';
+import TopBar from '@/components/ui/topBar';
 
 export default function History() {
   return (
     <div>
       <TopBar title="주행 기록"></TopBar>
       <HistorySearchBox />
-      <HistoryListBox />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          position: 'relative',
+        }}
+      >
+        <HistoryListBox />
+      </div>
     </div>
   );
 }
