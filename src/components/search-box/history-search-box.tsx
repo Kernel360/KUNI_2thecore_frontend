ef9255c4140b78fd527bf5f0cd6a791c53393595
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-05 12:31:15 +0900

    캘린더 시작일, 종료일 구분

--- src/components/search-box/history-search-box.tsx
+++ src/components/search-box/history-search-box.tsx
@@ -5,11 +5,10 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
-import Image from 'next/image';
 import { Button } from '../ui/button';
 import BrandFilterBox from './filter-box';
 import NumberSearchBox from './number-search-box';
-import { RangeCalendar } from './range-calendar';
+import { RangeCalendarFrom, RangeCalendarTo } from './range-calendar';
 import styles from './search-filter.module.css';
 
 const HistorySearchBox = () => {
@@ -17,22 +16,8 @@ const HistorySearchBox = () => {
     <div className="p-5">
       <div className="flex flex-row justify-between items-center gap-4 w-full max-w-[1200px] mx-auto">
         <NumberSearchBox />
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button className="flex bg-white w-[200px] h-11 border-2 border-[#d3d3d3] rounded-md items-center justify-between px-4 text-[#666] text-sm">
-              <span>조회 기간</span>
-              <Image
-                src="/calendar_month_24dp_434343.svg"
-                alt="calendar"
-                width={20}
-                height={20}
-              />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
-            <RangeCalendar />
-          </PopoverContent>
-        </Popover>
+        <RangeCalendarFrom />
+        <RangeCalendarTo />
         <Button className={styles.searchButton}>검색</Button>
       </div>
       <div className="flex flex-row justify-between items-center gap-4 w-full max-w-[1200px] mx-auto mt-5">
--- src/components/search-box/range-calendar.tsx
+++ src/components/search-box/range-calendar.tsx
@@ -1,24 +1,76 @@
 'use client';
 
 import * as React from 'react';
-import { DateRange } from 'react-day-picker';
 
+import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import Image from 'next/image';
 
-export function RangeCalendar() {
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: new Date(2025, 6, 5), // July 5, 2025
-    to: new Date(2025, 7, 23), // August 3, 2025
-  });
+export function RangeCalendarFrom() {
+  const [open, setOpen] = React.useState(false);
+  const [date1, setDate1] = React.useState<Date | undefined>(undefined);
 
   return (
-    <Calendar
-      mode="range"
-      defaultMonth={date?.from}
-      numberOfMonths={2}
-      selected={date}
-      onSelect={setDate}
-      className="rounded-lg border shadow-sm"
-    />
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="outline" id="date1" className="w-48 justify-between">
+          {date1 ? date1.toLocaleDateString() : '시작일'}
+          <Image
+            src="/calendar_month_24dp_434343.svg"
+            alt="calendar"
+            width={20}
+            height={20}
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto overflow-hidden p-0 bg-white" align="start">
+        <Calendar
+          mode="single"
+          selected={date1}
+          captionLayout="dropdown"
+          onSelect={date1 => {
+            setDate1(date1);
+            setOpen(false);
+          }}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export function RangeCalendarTo() {
+  const [open, setOpen] = React.useState(false);
+  const [date2, setDate2] = React.useState<Date | undefined>(undefined);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="outline" id="date2" className="w-48 justify-between">
+          {date2 ? date2.toLocaleDateString() : '종료일'}
+          <Image
+            src="/calendar_month_24dp_434343.svg"
+            alt="calendar"
+            width={20}
+            height={20}
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto overflow-hidden p-0 bg-white" align="start">
+        <Calendar
+          mode="single"
+          selected={date2}
+          captionLayout="dropdown"
+          onSelect={date2 => {
+            setDate2(date2);
+            setOpen(false);
+          }}
+        />
+      </PopoverContent>
+    </Popover>
   );
 }
