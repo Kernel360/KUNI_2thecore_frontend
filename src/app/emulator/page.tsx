890b19ebe91207375d96c43d8205df9099f40e87
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-06 22:44:39 +0900

    emulator UI 수정

--- src/app/emulator/page.tsx
+++ src/app/emulator/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import IconButton from '@/components/icon-button/icon-button';
+import NumberSearchBox from '@/components/search-box/number-search-box';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import {
@@ -53,28 +54,17 @@ const dummyEmuls = [
   },
 ];
 
-const CarEmulNumberSearchBox = () => {
+const EmulSearchBox = () => {
   return (
-    <div className={styles.searchContainer}>
-      <div className={styles.searchRow}>
-        <Input
-          type="text"
-          placeholder="차량 번호 (예: 11가 1111)"
-          className={styles.searchInput}
-        />
-        <Button className={styles.searchButton}>
-          검색
-        </Button>
-      </div>
-      <div className={styles.searchRow}>
+    <div className={styles.emulSearch}>
+      <NumberSearchBox />
+      <div className={styles.filterContainer}>
         <Input
           type="text"
           placeholder="새 에뮬레이터를 등록하려면 차량 번호를 이곳에 입력해주세요. (예: 11가 1111)"
-          className={styles.searchInput}
+          className={styles.searchContainer}
         />
-        <Button className={styles.searchButton}>
-          등록
-        </Button>
+        <Button className={styles.searchButton}>등록</Button>
       </div>
     </div>
   );
@@ -84,20 +74,14 @@ export default function Emulator() {
   return (
     <div>
       <TopBar title="에뮬레이터"></TopBar>
-      <CarEmulNumberSearchBox />
+      <EmulSearchBox />
       <Table className={styles.emulatorTable}>
         <TableHeader className={styles.tableHeader}>
           <TableRow>
             <TableHead className={styles.tableCellSmall}></TableHead>
-            <TableHead className={styles.tableCell}>
-              차량번호
-            </TableHead>
-            <TableHead className={styles.tableCell}>
-              에뮬레이터 ID
-            </TableHead>
-            <TableHead className={styles.tableCell}>
-              ON/OFF
-            </TableHead>
+            <TableHead className={styles.tableCell}>차량번호</TableHead>
+            <TableHead className={styles.tableCell}>에뮬레이터 ID</TableHead>
+            <TableHead className={styles.tableCell}>ON/OFF</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -124,4 +108,3 @@ export default function Emulator() {
     </div>
   );
 }
-
