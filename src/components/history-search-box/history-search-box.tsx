7fa8d2628bb9d3b166343b4919acb5b2008ade0c
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-12 18:04:48 +0900

    UI: 엑셀 버튼 그림자 수정

--- src/components/history-search-box/history-search-box.tsx
+++ src/components/history-search-box/history-search-box.tsx
@@ -165,7 +165,7 @@ const HistorySearchBox = ({
         />
         <Button
           className="w-40 h-11 mt-3 ml-0 mr-3 bg-gradient-to-br from-green-600 to-green-700 text-white text-sm font-semibold border-0
-          rounded-xl shadow-lg shadow-sky-600/30 transition-all duration-300 ease-in-out cursor-pointer hover:shadow-lg hover:shadow-green-800/40 active:scale-95 hover:-translate-y-1"
+          rounded-xl shadow-lg shadow-green-600/30 transition-all duration-300 ease-in-out cursor-pointer hover:shadow-lg hover:shadow-green-800/40 active:scale-95 hover:-translate-y-1"
         >
           엑셀 다운로드
         </Button>
