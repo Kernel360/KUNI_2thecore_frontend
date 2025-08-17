d9efc8ccd2436db0811e9c429a450a22aa60d155
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-17 19:09:14 +0900

    지도 확대 모달창 버튼 이동

--- src/components/menu-box/menu-box.tsx
+++ src/components/menu-box/menu-box.tsx
@@ -2,16 +2,9 @@ import { Link } from 'react-router-dom';
 import { Button } from '../ui/button';
 import styles from './menu-box.module.css';
 
-interface MenuBoxProps {
-  onOpenMapModal: () => void;
-}
-
-const MenuBox = ({ onOpenMapModal }: MenuBoxProps) => {
+const MenuBox = () => {
   return (
     <div className={'flex justify-start flex-col gap-2 h-full'}>
-      <Button className={styles.Button} onClick={onOpenMapModal}>
-        🗺️ 지도 전체 화면
-      </Button>
       <Link to="/search">
         <Button className={styles.Button}>🚗 차량 검색</Button>
       </Link>
