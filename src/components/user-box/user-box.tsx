7836163758d0073708803c0e5ef4620e85934026
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-18 00:19:45 +0900

    관리자 로그아웃 UI 생성

--- src/components/user-box/user-box.tsx
+++ src/components/user-box/user-box.tsx
@@ -1,13 +1,33 @@
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import styles from './user-box.module.css';
+
+export function AccountDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">👤 관리자 님</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className={styles.Button}>로그아웃</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 const UserBox = () => {
   return (
     <div className={styles.div}>
-      <h5>관리자님</h5>
-      <div className="flex justify-center">
-        <Button className={styles.Button}>로그아웃</Button>
-      </div>
+      <AccountDropdown />
     </div>
   );
 };
