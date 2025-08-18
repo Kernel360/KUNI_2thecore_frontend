944b801f1037b9d80b6f91b36d2d3419fb834684
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-18 15:40:04 +0900

    메뉴 완성

--- src/app/emulator/page.tsx
+++ src/app/emulator/page.tsx
@@ -1,4 +1,3 @@
-import MenuBox from '@/components/menu-box/menu-box';
 import NumberSearchBox from '@/components/search-box/number-search-box';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
@@ -10,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import TopBar from '@/components/ui/topBar';
 import { Car, CarSearchParams, CarService } from '@/services/car-service';
 import { useEffect, useState } from 'react';
 import styles from './emulator.module.css';
@@ -162,7 +160,6 @@ export default function LocalEmulator() {
 
   return (
     <div>
-      <TopBar title="에뮬레이터"></TopBar>
       <div className="gap-6 p-4 h-full w-[98%] mx-auto">
         <NumberSearchBox
           value={carNumber}
