c20b047af94150be399b710c50533828ea48bf6e
suuuujinnnn <suuuujinnnn@gmail.com>
2025-08-22 13:42:17 +0900

    회원가입 위치 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -90,6 +90,11 @@ export default function Login() {
         <CardHeader>
           <CardTitle>로그인</CardTitle>
           <CardDescription>아이디와 비밀번호를 입력하세요.</CardDescription>
+          <CardAction className="flex-end">
+            <Button variant="link" onClick={() => setIsModalOpen(true)}>
+              회원가입
+            </Button>
+          </CardAction>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit}>
@@ -126,9 +131,6 @@ export default function Login() {
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <CardAction className="flex-col gap-2">
-            <Button variant="link" onClick={() => setIsModalOpen(true)}>회원가입</Button>
-          </CardAction>
           <p>새 계정 등록 문의: ooo@oooo.com</p>
         </CardFooter>
       </Card>
