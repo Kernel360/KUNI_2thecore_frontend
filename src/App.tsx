c549dda56a6ba71325fc917aa924c02043db4e49
이총명 <dlchdaud04@naver.com>
2025-08-22 17:52:05 +0900

    feat: 4가지 api 1차적 개발

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
   const location = useLocation();
-  const isFloatingPage =
-    location.pathname === '/history' || location.pathname === '/search';
 
   return (
     <div className="flex flex-col h-screen">
@@ -13,8 +11,7 @@ function App() {
       <MenuBox />
       <div className="mx-auto flex flex-col justify-center items-center w-full flex-1 mt-2">
         <div
-          className={`bg-[#f6f6f6] pt-0 px-2.5 pb-6 mt-[3px] w-full h-fit max-w-[1290px] rounded-[10px] ${
-            isFloatingPage ? 'mb-6' : ''
+          className={`bg-[#f6f6f6] pt-0 px-2.5 pb-6 mt-[3px] w-full h-fit max-w-[1290px] rounded-[10px] mb-6
           }`}
         >
           <Outlet />
